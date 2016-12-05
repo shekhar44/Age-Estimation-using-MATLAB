e362555877b9a33a9289c015f2c80ace800e350c
shitanshu shekhar <shitanshushekhar95@yahoo.com>
2016-12-05 17:36:07 +0530

    Update train.m

--- train.m
+++ train.m
@@ -3,8 +3,7 @@ var=cd;                                                         % it store the c
 path=strcat(var,'\database\train\');
 d=dir(path);                                                    % list all directry in the folder
 k=1;c=0;
-    
-       for i=3:length(d)                                            % this loop used to reach to the training folder
+   for i=3:length(d)                                            % this loop used to reach to the training folder
        dir_name = d(i).name;
        path_name = strcat(path,dir_name,'\');
        d1 = dir(path_name);
@@ -14,21 +13,22 @@ k=1;c=0;
              image_path = strcat(path_name,image_name);
        im = imread(image_path);                                 % to read the image
           %figure;  imshow(im);
-      im = rgb2gray(im);                                       %convert the rbg pic to grayscale(black & white) for better result
+       im = rgb2gray(im);                                       %convert the rbg pic to grayscale(black & white) for better result
         imshow(im);hold on;                                     % to show the image
        points = detectSURFFeatures(im,'MetricThreshold',100);   % to detect the feature of figure
         %points1 = detectMinEigenFeatures(im);
-      points = points.selectStrongest(50);
-       features = extractFeatures(im,points); 
+      points = points.selectStrongest(60);
+       features = extracttFeatures(im,points); 
        f=features(:);                                           % collect all fearure in a single column
        f1(k,:)=f';                                              % transpose of matrix
        class(k,:) = c;
        k=k+1;
        plot(points);                                            % plot the point on figure
        end
-       c=c+1; 
-       end
-%sv=svmtrain(f1,class);                                          % include the feature and class file
-%save('sv.mat','sv');                                            % to save the sv
-display('WORK COMPLETED');                                      % display comment
+           c=c+1; 
            
+    end   
+sv=svmtrain(f1,class);                                          % include the feature and class file
+save('sv.mat','sv');                                            % to save the sv
+display('WORK COMPLETED');                                      % display comment 
+end
